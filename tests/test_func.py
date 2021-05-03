9179898783eba2816cc79d712ed41b9fa9f572f0
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-05-04 03:34:55 +0900

    test_meow added in test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -7,6 +7,9 @@ def dog(x, y):
 def woof(a):
     return "woof"
 
+def meow(a, b, c):
+    return "meow" if (a + b + c) > 0 else "bark"
+
 def test_cat():
     assert cat(5, 15) == 20
     assert cat("hello", "world") == "helloworld"
@@ -20,3 +23,8 @@ def test_dog():
 def test_woof():
     assert woof(1239) == "woof"
     assert woof("hello world") == "woof"
+
+def test_meow():
+    assert meow(-1, 9, -8) == "bark"
+    assert meow(-9, -12, -4) == "bark"
+    assert meow(0, 0, 183) == "meow"
